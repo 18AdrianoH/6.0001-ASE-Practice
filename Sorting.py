804937dc7a4b2ec3748dff31173491d8fcbb0076
18adrianoh <adrianohernandez2000gmail.com>
2018-08-30 17:59:56 -0400

    first sorting method
    still need to fix functional aspects that I'm not super used to

---
+++ Sorting.py
@@ -0,0 +1,85 @@
+'''
+Encapsulator for various static sorting methods.
+'''
+class Sorter(object):
+
+    '''
+    Initialization of the object, though it's unlikely that anyone will use it since
+    the methods are static.
+    '''
+    def __init__(self):
+        pass
+    '''
+    All sorted methods are highly imperative changing the actual ls object.
+    To get something that returns a sorted version of your ls please use the unique_sorted()
+    function with your favorite sorting function.
+
+    You can turn on the debug parameter/flag to tell the function that you want helpful print statements
+    of time and processing to help understand how runtime and memory usage as well as execution are going.
+    '''
+    # TODO: ADD DIFFERENT COMPARISON FUNCTIONS
+
+    '''
+    The good 'ol quicksort.
+    '''
+    @staticmethod
+    def quick_sort(ls,debug=False):
+        pass
+    
+    '''
+    Divide and conquer version of selection sort that works in nlogn since
+    for n items in a tree it must go up logn levels and do this for each item.
+    '''
+    @staticmethod
+    def merge_sort(ls,debug=False):
+        pass
+    
+    '''
+    Swap items if not in order... repeat until sorted.
+    '''
+    @staticmethod
+    def bubble_sort(ls,debug=False):
+        pass
+    
+    '''
+    Sort by inserting items where they belong... so like 9 8 3 4 -> 3 9 8 4 - > 3 4 9 8 - > 3 4 8 9
+    '''
+    @staticmethod
+    def insertion_sort(ls,debug=False):
+        pass
+
+    '''
+    Sort by selecting the smallest item and putting it at the front.
+    '''
+    @staticmethod
+    def selection_sort(ls,debug=False):
+        lls = len(ls)
+        for index in range(0,lls):
+            min_index = index
+            for mini in range(index+1,lls):
+                if (ls[mini] < ls[min_index]):
+                    min_index = mini
+                    pass
+                pass
+            (ls[index], ls[min_index]) = (ls[min_index], ls[index])
+            pass
+        pass
+    
+    '''
+    Heapify and sort that way.
+    '''
+    @staticmethod
+    def heap_sort(ls):
+        pass
+    
+    '''
+    Sorted returns a sorted version of the function.
+    You must pass your sort function from one of the above and your list
+    (or your function must be identical in API to the ones above.)
+    '''
+    @staticmethod
+    def unique_sorted(sort_function,ls,db=False):
+        sorted_ls = ls[:] # Here you can also use = ls.copy()
+        sort_function(ls,debug=db)
+        return sorted_ls
+    pass
---
+++ Tester.py
@@ -0,0 +1,18 @@
+from Sorting import Sorter
+'''
+Sorting Tester.
+'''
+# sort testing here
+# figure out python 3 print tricks
+
+ls = [1,3,5,4,2,7,5,0,9]
+print(ls)
+# TODO: PLEASE FIX THIS FUNCTIONAL FUNCTION
+sls = ls[:]#Sorter.unique_sorted(Sorter.selection_sort,ls) 
+#Sorter.selection_sort(sls)
+print(sls)
+
+'''
+Python idiosyncracies.
+'''
+# mess around here.
